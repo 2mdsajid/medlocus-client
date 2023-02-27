b0f56434b4935cfc879903e8d1bf8a6f93529277
2mdsajid <69730158+2mdsajid@users.noreply.github.com>
2023-02-27 23:17:16 +0545

    removed cors

--- src/components/med/UserLogin.js
+++ src/components/med/UserLogin.js
@@ -20,7 +20,6 @@ function UserLogin() {
     try {
 
       const res = await fetch(ROOT+"/userlogin", {
-        mode: 'no-cors',
         method: "POST",
         headers: {
           "Content-Type": "application/json"
